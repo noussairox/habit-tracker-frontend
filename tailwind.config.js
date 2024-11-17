e78ae750ce60b00a390204b257729153e2559ced
noussairox <abellouch34@gmail.com>
2024-11-17 18:33:09 +0100

    Page Home crée

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Cela dit à Tailwind de scanner tous les fichiers HTML et TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
